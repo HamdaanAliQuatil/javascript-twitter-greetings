a2029524a8cb898368dc17874e6d2115988476db
HamdaanAliQuatil <96776914+HamdaanAliQuatil@users.noreply.github.com>
2022-11-29 12:53:41 +0530

    feat: add greeting text to location paramter of profile

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 // importing necessary modules
 import Twitter from 'twitter';
 import dotenv from 'dotenv';
-import fetch from 'node-fetch';
 import { format } from 'date-fns';
 
 // loading environment variables
@@ -9,10 +8,21 @@ dotenv.config();
 
 // creating a new instance of Twitter
 const client = new Twitter({
-    consumer_key: process.env.CONSUMER_KEY,
-    consumer_secret: process.env.CONSUMER_SECRET,
-    access_token_key: process.env.ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
+// fetch the current day name
+const today = format(new Date(), 'EEEE');
 
+async function main(){
+    const params = {
+        location: 'Have a great ' + today + '!',
+    }
+    await client.post('account/update_profile', params);
+    console.log('Profile updated!');
+}
+
+main().catch(e => console.error(e));
